7dbc3cceada4e85869cab2ec182bd1a33172d763
Дима Бурлаков <tawerxo@mail.ru>
2025-07-19 16:24:34 +0300

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { TaskDetails } from "./pages/TaskDetails";
 import { LayoutWithTaskList } from "./pages/LayoutWithTaskList";
 
 const App = () => {
   return (
-    <BrowserRouter basename="t1-dz1-todolist">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LayoutWithTaskList />}>
           <Route path="/task/:id/edit" element={<TaskDetails mode="edit" />} />
           <Route path="task/new" element={<TaskDetails mode="new" />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
