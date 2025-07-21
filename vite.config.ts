273c1cfd3625926a950d441bc179da6e307cf69b
Дима Бурлаков <tawerxo@mail.ru>
2025-07-21 04:56:36 +0300

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "t1-dz1-todolist",
-  base: "/",
+  base: "t1-dz1-todolist",
   plugins: [react()],
 });
